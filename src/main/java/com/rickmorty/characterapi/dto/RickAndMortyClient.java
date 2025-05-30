8666545d0c5c49d9ca3f8432d1773886583c16ff
albertoochoagalvez <betangas8a@gmail.com>
2025-05-30 15:02:13 -0400

    clean spring boot project

--- src/main/java/com/rickmorty/characterapi/dto/RickAndMortyClient.java
+++
@@ -1,7 +0,0 @@
-package com.rickmorty.characterapi.dto;
-
-import org.springframework.web.client.RestTemplate;
-
-public class RickAndMortyClient {
-    private static final String BASE_URL = "https://rickandmortyapi.com/api/character";
-}
--- src/main/java/com/rickmorty/characterapi/repository/CharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rickmorty.characterapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.rickmorty.characterapi.model.Character;
-
-
-public interface CharacterRepository extends JpaRepository<Character, Long> {
-
-}
